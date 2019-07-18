5fcfa6a127fe9f90be69f66f2882b53b1008cba3
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-18 23:14:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musiciansArr, instrumentsArr){
+  let newArr = [];
+  let counter = 0;
+  for(let i = 0; i < musiciansArr.length; i++){
+    counter += i;
+  }
+}
